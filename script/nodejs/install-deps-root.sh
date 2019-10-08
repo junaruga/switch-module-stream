63bdb30d846e94b2d326e3eb992e39ce42bc01e6
Jan Staněk <jstanek@redhat.com>
2019-10-08 14:39:48 +0200

    Adapt NodeJS case for EL8 (#26)
    Signed-off-by: Jan Staněk <jstanek@redhat.com>

--- script/nodejs/install-deps-root.sh
+++ script/nodejs/install-deps-root.sh
@@ -4,6 +4,6 @@ set -ex
 . script/install-deps-root.sh
 
 # Install node package manager and nodegit build dependencies
-yum -y install npm node-gyp python krb5-devel
+yum -y install gcc-c++ npm krb5-devel python2
 
 node --version; npm --version
